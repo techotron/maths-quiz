6735031ed1b5adaaa58552b0f2ed946becc900d8
Edward Snow <edward.snow@disneystreaming.com>
2024-05-29 18:09:26 +0100

    page

---
+++ app/maths/multiplication/page.tsx
@@ -0,0 +1,11 @@
+import TimesTableTabs from "@/app/maths/multiplication/tabs"
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-4xl font-extrabold dark:text-white">Multiplication</h1>
+      <p>Learn how to multiply numbers.</p>
+      <TimesTableTabs />
+    </div>
+  )
+}
---
+++ app/maths/page.tsx
@@ -0,0 +1,19 @@
+import { inter } from '@/app/ui/fonts';
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>
+        Maths Quiz
+      </h1>
+      {/*<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">*/}
+      {/*  content placeholder*/}
+      {/*</div>*/}
+      {/*<div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">*/}
+      {/*  <div>*/}
+      {/*    content placeholder*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+    </main>
+  );
+}
